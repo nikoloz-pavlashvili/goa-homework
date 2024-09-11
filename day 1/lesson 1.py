dca8172bc7fdf9ca8dce901773105f12ed9fc617
nikoloz-pavlashvili <1508nikolozi@gmail.com>
2024-09-11 21:12:33 +0400

    commit

---
+++ day 1/lesson 1.py
@@ -0,0 +1,72 @@
+from turtle import *
+
+
+#step 1 i whant to build i house
+width(7)
+color ("purple")
+begin_fill ()
+forward (200)
+left (90)
+forward (200)
+left (90)
+forward (200)
+left (90)
+forward (200)
+left (90)
+forward (70)
+end_fill ()
+color ("yellow")
+begin_fill ()
+left (90)
+forward (120)
+right (90)
+forward (60)
+right (90)
+forward (120)
+end_fill ()
+
+penup ()
+goto (200 , 200)
+pendown ()
+color ("red")
+begin_fill ()
+right (150)
+forward (200)
+left (120)
+forward (200)
+end_fill ()
+
+penup ()
+goto (15 , 15)
+pendown ()
+
+
+color ("blue")
+begin_fill ()
+left (120)
+forward (30)
+right (360)
+left (90)
+forward (90)
+right (270)
+forward (30)
+left (90)
+forward (90)
+end_fill ()
+speed (50)
+penup ()
+goto (185 , 15)
+pendown ()
+
+
+
+
+
+
+
+
+
+
+
+
+exitonclick ()
