bbfe3dfd8f14c5a485b52561c8927e06d5593c73
nikoloz-pavlashvili <1508nikolozi@gmail.com>
2024-09-24 22:02:21 +0400

    commit

---
+++ day 3/homework/გოა/homework 3.py
@@ -0,0 +1,63 @@
+"""ვწერ ჩემი ოჯახის სახელს და გვარს"""
+
+
+
+#ვწერ ჩემი ოჯახის სახელებს
+
+print ("mother = NONA SHAVLIASHVILI")
+print("fhater = AVTO PAVLIASHVILI")
+print("bother = DANI PAVLIASHVILI")
+print("sister = LIZI WANTURISHVILI")
+print("grendmother = MERI ALELISHVILI,NANA XIDESHELI")
+print( "grendfather = GELA PAVLIASHVILI")
+
+
+# ვამრავლებ, ვყოფ , ვუმატებ და ვაკლებ რიცხვებს ერთმანეთს
+
+print ("1000098 + 23185")
+print ("4352233*23285172533")
+print ("62735233532-3372423358")
+print ("9252338325338/25652823")
+print ("2253382412273/933522835213")
+print ("2226243422624*622322725855")
+print("553722122908 +91522733o15222522")
+print("922373525652635 - 20545343433466")
+
+
+
+
+
+
+
+
+
+from turtle import *
+
+#მინდა რომ შევქმნა 2 სარტულიანი სახლი
+color ("purple")
+width (8)
+begin_fill ()
+
+forward (200)
+left (90)
+forward (200)
+left (90)
+forward (200)
+left (90)
+forward (200)
+left (90)
+right (90)
+forward (200)
+left (90)
+forward (200)
+left (90)
+forward (200)
+left (90)
+right(90)
+forward (200)
+left (45)
+forward (50)
+left (45)
+forward (130)
+left (45)
+forward (50)
